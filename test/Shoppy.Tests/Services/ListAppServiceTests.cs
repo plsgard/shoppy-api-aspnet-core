df85a06f91624cda46258a8b07a0bc4076cf9d80
Pier-Lionel Sgard <hello@plsgd.com>
2018-03-28 09:11:32 +0200

    Add test on service

--- test/Shoppy.Tests/Services/ListAppServiceTests.cs
+++ test/Shoppy.Tests/Services/ListAppServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Shoppy.Application.Lists;
@@ -73,6 +74,48 @@ namespace Shoppy.Tests.Services
 
             await UseDbContextAsync(async context =>
                 Assert.Equal(2, await context.Lists.CountAsync(i => i.Name == listName)));
+        }
+
+        [Fact]
+        public async Task Duplicate_WithItems_AllExistingAndNotPicked()
+        {
+            var userId = (await CreateUser()).Id;
+            LoginAs(userId);
+            var id = (await CreateList("liste1")).Id;
+            var item1 = await CreateItem(id, "item1", picked: true);
+            var item2 = await CreateItem(id, "item2");
+            var item3 = await CreateItem(id, "item3", picked: true);
+
+            var newListId = (await _listAppService.Duplicate(new DuplicateListDto
+            {
+                Name = "liste2",
+                ExistingListId = id
+            })).Id;
+
+            await UseDbContextAsync(async context =>
+            {
+                var list = await context.Lists.SingleOrDefaultAsync(c=>c.Id == newListId);
+                Assert.NotNull(list);
+                Assert.NotEqual(id, list.Id);
+                Assert.Equal("liste2", list.Name);
+
+                var newItems = await context.Items.Where(i => i.ListId == newListId).ToListAsync();
+                Assert.NotNull(newItems);
+                Assert.Equal(3, newItems.Count);
+
+                foreach (var newItem in newItems)
+                {
+                    var existingItem = new[] { item1, item2, item3 }.FirstOrDefault(i => i.Name == newItem.Name);
+                    Assert.NotNull(existingItem);
+                    Assert.False(newItem.Picked);
+                    Assert.NotNull(newItem.CreationUserId);
+                    Assert.NotEqual(DateTimeOffset.MinValue, newItem.CreationTime);
+                    Assert.Equal(userId, newItem.UserId);
+                    Assert.Equal(existingItem.Index, newItem.Index);
+                    Assert.Equal(newListId, newItem.ListId);
+                    Assert.NotEqual(existingItem.Id, newItem.Id);
+                }
+            });
         }
     }
 }
